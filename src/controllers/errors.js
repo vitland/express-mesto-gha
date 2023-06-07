ef1df4fd6a48f1451566e00b613f0dfb2b267020
Vitkor <vitland@gmail.com>
2023-06-07 23:45:40 +0300

    feat: Добавить обработчик ошибок

---
+++ src/controllers/errors.js
@@ -0,0 +1,17 @@
+module.exports = class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NotFound";
+    this.statusCode = 404;
+  }
+};
+
+module.exports.handleErrors = (err, res) => {
+  if (err.name === "NotFound" || err.name === "CastError") {
+    return res.status(404).send({ message: err.message });
+  }
+  if (err.name === "ValidationError") {
+    return res.status(400).send({ message: err.message });
+  }
+  res.status(500).send({ message: err.message });
+};
