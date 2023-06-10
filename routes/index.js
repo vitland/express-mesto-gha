b2867067e7713f2bc00cbb853ace71119e678305
Vitkor <vitland@gmail.com>
2023-06-11 00:34:04 +0300

    refactor: Перенести 404 route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 router.use("/users", require("./users"));
 router.use("/cards", require("./cards"));
-
+router
+.use((req, res, next) => {
+  return res.status(404).send({ message: "Страница не найдена" });
+});
 module.exports = router
